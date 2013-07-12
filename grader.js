d6c8748c528bf068ffc6089014d7a8aaf70bbe3a
Ubuntu <ubuntu@ip-172-31-10-81.us-west-2.compute.internal>
2013-07-12 05:39:45 +0000

    Completed HW3

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var checksfile = CHECKSFILE_DEFAULT;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,15 +46,25 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlString = function(s) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+        var present = s(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
+};
+
+var processString = function(s) {
+    var checkJson = checkHtmlString(s);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+    process.exit(0);
+};
+
+var processFile = function(htmlfile) {
+    processString(cheerioHtmlFile(htmlfile));
 };
 
 var clone = function(fn) {
@@ -61,14 +73,26 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var assertURLExists = function(url) {
+    rest.get(url).on('complete', function(result) {
+	if (result instanceof Error) {
+	    console.log("Unable to process url %s. Exiting.", url);
+	    process.exit(1);
+	} else {
+	    processString(cheerio.load(result));
+	}
+    })
+};
+    
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to test', clone(assertURLExists), '')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    checksfile = program.checks;
+    if (program.url == '')
+	processFile(program.file);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
